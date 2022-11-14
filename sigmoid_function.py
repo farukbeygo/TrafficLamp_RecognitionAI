d7ce8aa8adf8e88cc92809ea6e86fe1d7d1fb778
Faruk <109393466+farukbeygo@users.noreply.github.com>
2022-11-14 23:40:36 +0300

    Update sigmoid_function.py

--- sigmoid_function.py
+++ sigmoid_function.py
@@ -1,6 +1,19 @@
 import numpy as np
 
-# define sigmoid function for prediction
+# sigmoid function for binary result 0<x<1
 def sigmoid(x):
-    s = 1 / (1 + np.exp(-x))
-    return s
+    sigmoid = 1 / (1 + np.exp(-x))
+    return sigmoid
+
+# tanh function for hidden layers -1<x<1
+def tanh(x):
+    tanh = (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
+    return tanh
+
+# ReLU function 
+def ReLU(x):
+    if x>0:
+        rectifier = x
+    else:
+        rectifier = 0
+    return rectifier
